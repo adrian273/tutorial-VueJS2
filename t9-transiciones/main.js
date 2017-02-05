345eea568475bacb3ec395a2184901386d8e6d69
Adrian Verdugo <adrianverdugo273@gmail.com>
2017-02-05 17:19:23 -0300

    transciciones

---
+++ t9-transiciones/main.js
@@ -0,0 +1,12 @@
+const vm = new Vue({
+    el: 'main',
+    data: {
+        mostrar: true,
+        mensajes: {
+            transicion: 'trancision con vue js',
+            animacion: 'animacion css con vuejs',
+            animacionCustom: 'animaciones css con vue.js',
+            entreElementos: 'transiciones entre elemenentos con vue.js'
+        }
+    }
+});
