e214433873a1ded828c5846c6367ad55d89c7866
Adrian Verdugo <adrianverdugo273@gmail.com>
2017-03-08 22:48:59 -0300

    t26 render function

---
+++ t26-render_function/src/App.vue
@@ -0,0 +1,37 @@
+<style media="screen">
+h1{
+  color: red;
+}
+
+
+</style>
+
+<template id="">
+
+<div class="">
+  <elemento tipo="main">
+      <h1>Hola mundo main</h1>
+  </elemento>
+  <elemento tipo="footer">
+      <h1>Hola mundo footer</h1>
+  </elemento>
+  <elemento tipo="aside">
+      <h1>Hola mundo aside</h1>
+  </elemento>
+  <elemento tipo="section">
+      <h1>Hola mundo Vue</h1>
+  </elemento>
+</div>
+
+</template>
+
+<script>
+
+import elemento from './components/Elemento.vue';
+export default {
+    components: {
+        elemento
+    }
+}
+
+</script>
---
+++ t26-render_function/src/components/Elemento.vue
@@ -0,0 +1,33 @@
+<style lang="css">
+
+
+
+</style>
+<!--
+<template lang="html">
+
+<div class="">
+    <section v-if="tipo === 'section'">
+        <slot></slot>
+    </section>
+    <aside class="" v-if="tipo === 'aside'">
+        <slot></slot>
+    </aside>
+</div>
+
+</template>
+-->
+
+<script>
+
+export default {
+    props: ['tipo'],
+    render(createdElement) {
+        return createdElement (
+          this.tipo,
+          this.$slots.default,
+        )
+    }
+}
+
+</script>
