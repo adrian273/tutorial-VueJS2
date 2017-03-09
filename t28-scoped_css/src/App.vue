25160cc1fe26731b2fa6eac2437dbed4f3fbcc03
Adrian Verdugo <adrianverdugo273@gmail.com>
2017-03-09 12:53:44 -0300

    t28 scoped_css

---
+++ t28-scoped_css/src/App.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+    <div class="">
+      <h1>Menu principal</h1>
+      <input type="range" name="name" value="">
+      <ul>
+          <li><a href="#">Enlace 1</a></li>
+          <li><a href="#">Enlace 2</a></li>
+          <li><a href="#">Enlace 3</a></li>
+          <li><a href="#">Enlace 4</a></li>
+      </ul>
+      <tareas></tareas>
+    </div>
+</template>
+
+<script>
+import Tareas from './components/Tareas.vue';
+export default {
+    components: {Tareas}
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ t28-scoped_css/src/components/Tareas.vue
@@ -0,0 +1,70 @@
+
+<!-- agregar scoped en caso de querer estilos locals, solamente del compoenente -->
+<style lang="css" scoped>
+
+
+li a {
+    color: red;
+}
+
+</style>
+
+<template lang="html">
+
+<div class="">
+    <h2>Tareas que realizar</h2>
+    <ul>
+        <li v-for="tarea in tareasHacer">
+            <a href="#" @click="actualizarEstado(tarea)" v-text="tarea.nombre"></a>
+        </li>
+    </ul>
+    <h2>Tareas realizadas</h2>
+    <ul>
+        <li v-for="tarea in tareasFin">
+            <a href="#" @click="actualizarEstado(tarea)" v-text="tarea.nombre"></a>
+        </li>
+    </ul>
+</div>
+
+</template>
+
+<script>
+
+export default {
+    data() {
+            return {
+                tareas: [{
+                    nombre: "Aprender Vue.js ",
+                    completado: false
+                }, {
+                    nombre: "Aprender ES6",
+                    completado: false
+                }, {
+                    nombre: "Pintar GRAFF",
+                    completado: false
+                }, {
+                    nombre: "arreglar el router del GRN",
+                    completado: false
+                }, {
+                    nombre: "Pasar de codeigneter 3 a laravel para poder usar VUE.js",
+                    completado: false
+                }],
+                finalizado: []
+            }
+        },
+        methods: {
+            actualizarEstado(tarea) {
+                tarea.completado = !tarea.completado;
+            }
+        },
+        computed: {
+            tareasHacer() {
+                    return this.tareas.filter((t) => !t.completado);
+                },
+                tareasFin() {
+                    return this.tareas.filter((t) => t.completado);
+                }
+        }
+}
+
+</script>
