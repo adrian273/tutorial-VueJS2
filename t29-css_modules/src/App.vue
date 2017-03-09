b71235554a39cccfcc61a319a9459880b1b0f359
Adrian Verdugo <adrianverdugo273@gmail.com>
2017-03-09 13:23:35 -0300

    t29 css modules

---
+++ t29-css_modules/src/App.vue
@@ -0,0 +1,31 @@
+<style lang="css" module>
+
+.subrayado {
+    text-decoration: underline;
+}
+
+.rojo {
+    color: red;
+}
+
+</style>
+
+<template lang="html">
+    <div class="">
+      <h1 :class="[$style.rojo]">Este es un color Rojo</h1>
+      <h1 :class="{[$style.subrayado] : subrayado}">Hola css Modules</h1>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    data(){
+      return {
+            subrayado : true
+      }
+    }
+}
+
+</script>
