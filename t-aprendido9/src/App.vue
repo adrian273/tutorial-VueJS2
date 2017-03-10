ad92caad73071203707abc5f0ec12fa0d62a3dc2
Adrian Verdugo <adrianverdugo273@gmail.com>
2017-03-10 19:32:12 -0300

    t-aprendido 9, estado  de vuex

---
+++ t-aprendido9/src/App.vue
@@ -0,0 +1,10 @@
+<template lang="html">
+    <user></user>
+</template>
+
+<script>
+import User from './components/User.vue';
+export default {
+    components: {User}
+}
+</script>
---
+++ t-aprendido9/src/components/User.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+    <div :class="[$style.color]">
+        <h1>Nombre : {{name}}</h1>
+        <h2>Apellido : {{surname}}</h2>
+        <h3>Hobbys: {{hoobys}}</h3>
+        <h4>Años : {{age}}</h4>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+export default {
+    computed: mapState({
+        name: (state) => state.name,
+        surname: (state) => state.surname,
+        hoobys: (s) => s.hoobys,
+        age: (st) => st.age
+    }),
+}
+</script>
+
+<style lang="css" module>
+    .color h1{
+        color:red;
+    }
+    .color h2{
+        color: blue;
+    }
+    .color h3,h4{
+        color: green;
+    }
+</style>
