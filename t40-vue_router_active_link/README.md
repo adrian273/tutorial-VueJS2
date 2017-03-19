fc0299fd6c00bbcbc17dd8ee872b6b8be1071ed9
Adrian Verdugo <adrianverdugo273@gmail.com>
2017-03-18 22:05:29 -0300

    t40-vue_router_active_link

---
+++ t40-vue_router_active_link/README.md
@@ -0,0 +1,18 @@
+# t40-vue_router_active_link
+
+> cómo destacar visualmente la ruta activa en cada momento, gracias a la propiedad active-class active-class del componente de Vue Router: router-link.
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ t40-vue_router_active_link/src/App.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <section>
+      <h1>Rutas con Vue router</h1>
+      <router-link to='/'  tag='li' exact><a>Home</a></router-link>
+      <router-link to='/contacto' active-class='active' tag='li'><a>Contacto</a></router-link>
+      <hr>
+      <router-view></router-view>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+    .active a {
+        color: red;
+    }
+
+    .router-link-active a {
+        color: green;
+    }
+
+</style>
