2e9cee1f77334d773f6067a0a2860e18eaa5deff
Adrian Verdugo <adrianverdugo273@gmail.com>
2017-03-20 11:43:42 -0300

    t-aprendido16, vue-router

---
+++ t-aprendido16/src/App.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <div class="container-fluid">
+    <div class="row">
+        <nav class="navbar" role="navigation">
+        </nav>
+        <div class="col-md-3 col-lg-3">
+          <div class="container-fluid">
+              <ul class="nav nav-pills nav-stacked">
+                  <li role="presentation"><router-link active-class='active-link' to='/' exact>Home</router-link></li>
+                  <li role="presentation"><router-link to='/contacto' active-class='active-link' >Contacto</router-link></li>
+              </ul>
+          </div>
+        </div>
+        <div class="col-md-9 col-lg-9">
+            <router-view></router-view>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+    .active-link{
+        color: red;
+    }
+</style>
---
+++ t-aprendido16/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="panel panel-primary">
+      <div class="panel-heading">
+        <h3 class="panel-title">Home</h3>
+      </div>
+      <div class="panel-body">
+        Alerts
+
+        Provide contextual feedback messages for typical user actions with the handful of available and flexible alert messages.
+
+        Examples
+        Wrap any text and an optional dismiss button in .alert and one of the four contextual classes (e.g., .alert-success) for basic alert messages.
+      </div>
+      <div class="panel-footer">
+          adrian verdugo #VueJS
+      </div>
+    </div>
+</template>
