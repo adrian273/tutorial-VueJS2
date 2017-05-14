4bc2405fd0f9b5d10f802bfd75c3377fa5b92446
Adrian Verdugo <adrianverdugo273@gmail.com>
2017-05-14 09:17:17 -0400

    t54-vue_router_meta_fields

---
+++ t54-vue_router_meta_fields/README.md
@@ -0,0 +1,19 @@
+# t54-vue_router_meta_fields
+
+>  guardar información meta en cada una de las rutasen cada una de las rutas para luego utilizarla para validar el acceso.
+
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ t54-vue_router_meta_fields/src/components/User.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+    <section>
+        <h1>Usuario</h1>
+    </section>
+</template>
+
+<script>
+  export default {
+      beforeRouteEnter: ((to, from, next) => {
+          console.info('antes de entrar a usuario')
+          next((vm) => {
+              console.log(vm);
+          })
+      }),
+      beforeRouteUpdate: ((to, from, next) => {
+          console.info('antes de cambiar la ruta sobre el mismo componente')
+          console.log(this);
+      }),
+      beforeRouteLeave: ((to, from, next) => {
+            console.info('Antes de salir')
+            next()
+      }),
+  }
+</script>
