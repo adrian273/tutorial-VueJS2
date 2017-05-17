4ed892f8b08942e7285c50ecebc8efaae2d18002
Adrian Verdugo <adrianverdugo273@gmail.com>
2017-05-17 15:19:03 -0400

    t57-vue_router_data_fetch2

---
+++ t57-vue_router_data_fetch2/src/components/Contact.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="jumbotron">
+      <h1>Contacto: </h1>
+      <p>adrianverdugo273@gmail.com</p>
+      <pre>{{ numero }}</pre>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                numero: null
+            }
+        },
+        beforeRouteEnter: ((to, from ,next) => {
+            console.info('antes de entrar')
+            setTimeout(() => {
+                // this no va a funcionar porque no esta cargado. se utiliza un vm
+                next((vm) => {
+                    vm.numero = Math.floor(Math.random() * 1000);
+                })
+            }, 2000)
+        })
+    }
+</script>
---
+++ t57-vue_router_data_fetch2/src/routes.js
@@ -0,0 +1,41 @@
+import Home from './components/Home.vue'
+import Crew from './components/Crew.vue'
+import User from './components/User.vue'
+import UserBio from './components/UserBio.vue'
+import UserImg from './components/UserImg.vue'
+import UserFriend from './components/UserFriend.vue'
+import Contact from './components/Contact.vue'
+import Error404 from './components/Error404.vue'
+import { store } from './store'
+
+export const routes = [
+    {
+        path: '/', component: Home, name: 'home'
+    },
+    {
+        path: '/crew/:ident', component: Crew, children:[
+        {
+            path: '', component: User, children:[
+                {
+                  path: '', component: UserBio, name: 'bio'
+                },
+                {
+                    path: 'fotos', component: UserImg, name: 'fotos'
+                },
+                {
+                  path: 'amigos', component: UserFriend, name: 'friends'
+                }
+            ], name: 'crew'
+        }
+      ], props: true
+    },
+    {
+      beforeEnter:((to, from, next) => {
+          next(store.state.auth)
+      }), path: '/contact', component: Contact, name: 'contact'
+    },
+    // pagina no encontrada
+    {
+        path: '*', component: Error404
+    }
+];
